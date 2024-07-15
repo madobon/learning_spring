9d4404875390090426f2dfc672e58742364027c5
madobon <mimicopy@live.jp>
2024-07-15 11:04:42 +0900

    [spring practice]refactoring and add comments

--- src/main/java/com/example/its/domain/issue/IssueService.java
+++ src/main/java/com/example/its/domain/issue/IssueService.java
@@ -16,9 +16,9 @@ public class IssueService {
      */
     public List<IssueEntity> findAll(){
         return List.of(
-                new IssueEntity(1, "概要1", "説明1"),
-                new IssueEntity(2, "概要2", "説明2"),
-                new IssueEntity(3, "概要3", "説明3")
+            new IssueEntity(1, "概要1", "説明1"),
+            new IssueEntity(2, "概要2", "説明2"),
+            new IssueEntity(3, "概要3", "説明3")
         );
     }
 }
